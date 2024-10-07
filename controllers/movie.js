f55a93f791ed478cbfe74517373af089e6945cf9
dspodina <spodina.darya@gmail.com>
2024-10-07 12:21:24 +0200

    added routes update and delete

--- controllers/movie.js
+++ controllers/movie.js
@@ -1,6 +1,7 @@
 // import modules and dependencies
 import { v4 as Id } from 'uuid';
 import Movie from '../models/movie.js';
+import { title } from 'process';
 
 const movieControllers = {
     getAllMovies: (req, res) => {
@@ -20,7 +21,7 @@ const movieControllers = {
         }
     },
     addMovieForm: (req, res) => {
-        res.status(200).render('add-movie');
+        res.status(200).render('add-movie-form');
     },
     addMovie: (req, res) => {
         const { title, genre, year, img } = req.body;
@@ -31,6 +32,45 @@ const movieControllers = {
             res.status(400).render('404', {
                 title: 'All fields are required',
                 message: `Please, add name, genre and img`
+            });
+        }
+    },
+    updateMovieForm: (req, res) => {
+        const { id } = req.params;
+        const movie = Movie.getMovieById(id);
+        if (movie) {
+            res.status(200).render('update-movie-form', { movie });
+        } else {
+            res.status(404).render('404', {
+                title: 'Movie not found',
+                message: "The movie you're looking dosn't exist"
+            });
+        }
+    },
+    updateMovie: (req, res) => {
+        const { id } = req.params;
+        const { title, genre, year, img } = req.body;
+        const movie = Movie.getMovieById(id);
+        if (movie) {
+            Movie.updateMovie(id, { title, genre, year, img });
+            res.status(200).redirect('/api/movies');
+        } else {
+            res.status(404).render('404', {
+                title: 'Movie not found',
+                message: "The movie you're looking dosn't exist"
+            });
+        }
+    },
+    deleteMovie: (req, res) => {
+        const { id } = req.params;
+        const movie = Movie.getMovieById(id);
+        if (movie) {
+            Movie.deleteMovie(id);
+            res.status(200).redirect('/api/movies');
+        } else {
+            res.status(404).render('404', {
+                title: 'Movie not found',
+                message: "The movie you're looking dosn't exist"
             });
         }
     }
--- routes/movie.js
+++ routes/movie.js
@@ -3,12 +3,23 @@ import movieControllers from '../controllers/movie.js';
 
 const router = express.Router();
 
-const { getAllMovies, getMovieById, addMovieForm, addMovie } = movieControllers;
+const {
+    getAllMovies,
+    getMovieById,
+    addMovieForm,
+    addMovie,
+    updateMovieForm,
+    updateMovie,
+    deleteMovie
+} = movieControllers;
 
 // Movie routes
 router.get('/movies', getAllMovies);
 router.get('/movies/:id', getMovieById);
 router.get('/add-movie', addMovieForm);
 router.post('/add-movie', addMovie);
+router.get('/update-form/:id', updateMovieForm);
+router.post('/update/:id', updateMovie);
+router.get('/delete/:id', deleteMovie);
 
 export default router;
